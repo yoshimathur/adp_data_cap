1bfd5e090a47f4fc6d4c5cf5d3de9330750ed892
Yash Mathur <yashmathur@Yashs-MacBook-Pro.local>
2025-08-19 12:04:01 -0400

    don't merge feedback

--- preprocess/loader.py
+++ preprocess/loader.py
@@ -11,12 +11,15 @@ usage_df = pd.read_csv('data/usage_data.csv')
 
 # merge benefits metadata onto usage
 merge1_df = pd.merge(usage_df, benefits_df, on="BenefitID", how="left")
+print(len(merge1_df))
 
 # merge feedback onto previously merged usage
-merge2_df = pd.merge(merge1_df, feedback_df, on=["EmployeeID", "BenefitID"], how="left", validate="many_to_many")
+# merge2_df = pd.merge(merge1_df, feedback_df, on=["EmployeeID", "BenefitID"], how="outer")
+# print(len(merge2_df))
 
 # merge complete benefit data onto employee
-final_df = pd.merge(employee_df, merge2_df, on="EmployeeID", how="left")
+final_df = pd.merge(employee_df, merge1_df, on="EmployeeID", how="left")
+print(len(final_df))
 
 
 DATA = final_df
