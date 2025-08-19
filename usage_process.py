7b1a9c56101b0751332d433134c721a9314a30f2
frogchewer <yyaragarla@gmail.com>
2025-08-19 15:47:21 -0400

    usage categories

--- usage_process.py
+++ usage_process.py
@@ -5,3 +5,4 @@ def average_utilization(col_name, usage):
     mean = usage[[col_name, 'UsageFrequency']].groupby(col_name).mean('UsageFrequency')
     plt.bar(mean.index, mean['UsageFrequency'])
     return mean
+
