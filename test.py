fd83ad1df41cf37ea62dd972cb5039f3ed7cfb27
Yash Mathur <yashmathur@Yashs-MacBook-Pro.local>
2025-08-21 14:39:44 -0400

    Usage Cost Deltas

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
-from utils.feedback import get_feedback
-from models.collab_filter import get_collab_recommendations
+from utils.loader import EMPLOYEES_BENEFITS_USAGE
+import utils.col_refs as REF
+from models.collab_filter import get_collab_recommendations, create_embeddings, BENEFITS_VECTORIZER
 
-get_collab_recommendations(4245)
+employees = EMPLOYEES_BENEFITS_USAGE[REF.employeeID].unique().tolist()
+embeddings = create_embeddings()
+
+delta = get_collab_recommendations(employees[1], embeddings)
+
+for i, benefit_delta in enumerate(delta): 
+    print(BENEFITS_VECTORIZER[i], benefit_delta)
